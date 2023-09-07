570206a6a94baceb86b04cabd383942d90befdd4
Chad96 <chadrackndalamba@gmail.com>
2023-09-07 20:42:11 +0200

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-/**main:all the contents are stored in the main function
+/**
+ * main - all the contents are stored in the main function
  * return 0:the programme must return 0 at the end
- */
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
