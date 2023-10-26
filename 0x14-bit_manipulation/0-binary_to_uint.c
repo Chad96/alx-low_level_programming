9c7cb6d07d2d15b1f83d58321989ad5a9989836b
Chad96 <124060913+Chad96@users.noreply.github.com>
2023-10-26 12:57:58 +0200

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,1 +1,27 @@
 #include "main.h"
+/**
+ * set_bit - Sets the value of a bit at a given index to 1.
+ * @n: A pointer to an unsigned long integer.
+ * @index: The index of the bit to set, starting from 0.
+ *
+ * Return: 1 if it worked, or -1 if an error occurred.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+    unsigned long int mask;
+
+    if (index >= 64)  // Assuming a 64-bit system
+        return -1;
+
+    mask = 1UL << index;
+    *n = *n | mask;
+
+    return 1;
+}
+By moving the variable declaration of mask to the beginning of the function, you resolve the "ISO C90 forbids mixed declarations and code" error. This code should compile without errors.
+
+
+
+
+
+
