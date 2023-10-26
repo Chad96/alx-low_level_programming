34b7407994ec76cad9b4f00fd6fc38d61bd8cac6
Chadrack Ndalamba <chadrackndalamba@gmail.com>
2023-10-26 03:48:32 -0700

    task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,18 +1,1 @@
 #include "main.h"
-
-/**
- * set_bit - Sets the value of a bit at a given index to 1.
- * @n: A pointer to an unsigned long integer.
- * @index: The index of the bit to set, starting from 0.
- *
- * Return: 1 if it worked, or -1 if an error occurred.
- */
-int set_bit(unsigned long int *n, unsigned int index)
-{
-	if (index >= 64)
-		return -1;
-	
-	unsigned long int mask = 1UL << index;
-	*n = *n | mask;
-	return (1);
-}
