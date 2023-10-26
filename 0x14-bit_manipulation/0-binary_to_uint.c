ffba6cf2bf65efeaca176b7019ef2d1303620c83
Chad96 <124060913+Chad96@users.noreply.github.com>
2023-10-26 12:24:51 +0200

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,2 +1,22 @@
 #include "main.h"
+#include "main.h"
+
+/**
+ * set_bit - Sets the value of a bit at a given index to 1.
+ * @n: A pointer to an unsigned long integer.
+ * @index: The index of the bit to set, starting from 0.
+ *
+ * Return: 1 if it worked, or -1 if an error occurred.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+    if (index >= 64)  // Assuming a 64-bit system
+        return -1;
+
+    unsigned long int mask = 1UL << index;
+    *n = *n | mask;
+
+    return 1;
+}
+
 
