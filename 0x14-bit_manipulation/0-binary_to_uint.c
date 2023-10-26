f77ede24959eaf47a01dfbfcc1ba2a3ceb1b4bac
Chad96 <124060913+Chad96@users.noreply.github.com>
2023-10-26 13:10:59 +0200

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,18 +1,29 @@
 #include "main.h"
-/**
- * set_bit - Sets the value of a bit at a given index to 1.
- * @n: A pointer to an unsigned long integer.
- * @index: The index of the bit to set, starting from 0.
- *
- * Return: 1 if it worked, or -1 if an error occurred.
- */
-int set_bit(unsigned long int *n, unsigned int index)
-{
-	unsigned long int mask;
 
-	if (index >= 64)
-		return (-1);
-	mask = 1UL << index;
-	*n = *n | mask;
-	return (1);
+/**
+ * binary_to_uint - function converts a binary number to an unsigned int.
+ * @b: pointer to a string containing a binary number
+ *
+ * Return: unsigned int with decimal value of binsry number, or 0 if error
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int a;
+	unsigned int num;
+
+	num = 0;
+	if (!b)
+		return (0);
+	for (a = 0; b[a] != '\0'; a++)
+	{
+		if (b[a] != '0' && b[a] != '1')
+			return (0);
+	}
+	for (a = 0; b[a] != '\0'; a++)
+	{
+		num <<= 1;
+		if (b[a] == '1')
+			num += 1;
+	}
+	return (num);
 }
