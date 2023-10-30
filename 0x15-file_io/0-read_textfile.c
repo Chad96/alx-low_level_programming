f7a6090dc0104dfc42b2480b5dc92e1616510566
Chadrack Ndalamba <chadrackndalamba@gmail.com>
2023-10-30 10:14:37 -0700

    tasks

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,47 +1,56 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+/**
+ * main - caries all the body functions in the curly braces
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	if (filename == NULL)
+		return (0);
 
-ssize_t read_textfile(const char *filename, size_t letters) {
-    if (filename == NULL)
-        return 0;
+	int fd = open(filename, O_RDONLY);
 
-    int fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return 0;
+	if (fd == -1)
+		return (0);
 
-    char buffer[letters];
-    ssize_t bytes_read = read(fd, buffer, letters);
+	char buffer[letters];
+	ssize_t bytes_read = read(fd, buffer, letters);
 
-    if (bytes_read == -1) {
-        close(fd);
-        return 0;
-    }
+	if (bytes_read == -1)
+	{
+		close(fd);
+		return (0);
+	}
+	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 
-    ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-
-    if (bytes_written == -1 || bytes_written != bytes_read) {
-        close(fd);
-        return 0;
-    }
-
-    close(fd);
-    return bytes_written;
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		close(fd);
+		return (0);
+	}
+	close(fd);
+	return (bytes_written);
 }
+int main(int ac, char **av)
+{
+	ssize_t n;
 
-int main(int ac, char **av) {
-    ssize_t n;
-
-    if (ac != 2) {
-        dprintf(2, "Usage: %s filename\n", av[0]);
-        exit(1);
-    }
-
-    n = read_textfile(av[1], 114);
-    printf("\n(printed chars: %li)\n", n);
-    n = read_textfile(av[1], 1024);
-    printf("\n(printed chars: %li)\n", n);
-
-    return 0;
+	if (ac != 2)
+	{
+		dprintf(2, "Usage: %s filename\n", av[0]);
+		exit(1);
+	}
+	n = read_textfile(av[1], 114);
+	printf("\n(printed chars: %li)\n", n);
+	n = read_textfile(av[1], 1024);
+	printf("\n(printed chars: %li)\n", n);
+	return (0);
 }
